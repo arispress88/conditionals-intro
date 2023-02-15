3565a109a3120aee53ddcf82c84125e07a01c6f9
Arnold Rispress <arnold.rispress@gmail.com>
2023-02-15 10:37:53 -0500

    Condtionals like a boss!

---
+++ main.js
@@ -0,0 +1,58 @@
+if(1+1 > 5){
+    console.log("True fact!")
+} else {
+    console.log("LIES!")
+}
+
+if(4*5 <= 20){
+    console.log("True fact!")
+} else {
+    console.log("LIES!")
+}
+
+if(6/2 >= 0){
+    console.log("True fact!")
+} else {
+    console.log("LIES!")
+}
+
+var likesDogs = true;
+if (likesDogs === true){
+    console.log("You're a dog person!")
+} else{
+    console.log("You're a cat person!")
+}
+
+var name = "William";
+var English = true;
+
+if(name === "William" && English){
+    console.log(`Hello ${name}!`)
+}
+
+var nameTwo = "Guillaume"
+var French = true;
+
+if(nameTwo = "Guillaume"){
+    console.log(`Bonjour ${nameTwo}!`)
+}
+
+var nameThree = "Guillermo"
+var Spanish = true;
+
+if(nameThree = "Guillermo"){
+    console.log(`Hola ${nameThree}!`)
+}
+
+var grade = 98;
+if(grade <=69){
+    console.log("You got an F")
+} else if(grade <=76){
+    console.log("You got a D")
+} else if(grade <=84){
+    console.log("You got a C")
+} else if(grade <=92){
+    console.log("You got a B")
+} else if(grade <=100){
+    console.log("You got an A")
+}
